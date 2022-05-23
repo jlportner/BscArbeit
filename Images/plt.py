1ec28672afcc386ff2c7fa100642e194287caf0c
jlportner <jean-luc@portner.at>
2022-05-23 23:00:39 +0200

    23.05.2022

--- Images/plt.py
+++ Images/plt.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.arange(26)
+t = np.arange(1,26)
+t2 = t.astype("float")
+t2[0] = 5/3
 
-vcd = 1/2 * (t+3)
-stb = 5/4 * (t+1)
+vcd = 2 * t2 - 3
+stb = 4/5 * t2 - 1
 
-moritaC = np.array([(4,4),(8,6),(12,8)])
-morita = np.array([(16,10),(20,12),(24,14)])
-bartholdi = np.array([(8,7),(11,7)])
+moritaC = np.array([(4,4),(6,8),(8,12)])
+morita = np.array([(10,16),(12,20),(14,24)])
+bartholdi = np.array([(7,8),(7,11)])
+
 fig = plt.figure(figsize=2.5*np.array([3.74,2.4]))
 plt.rcParams.update({
   "text.usetex": True,
@@ -18,29 +21,34 @@ plt.rcParams.update({
   "font.size": 16
 })
 
-orange = [1.0,0.7,0.627,0.5]
-plt.fill_between(t,vcd,stb,color=[0.7,0.7,0.7,0.3],zorder=1.0)
-plt.plot(t,vcd,c='black',zorder=2.0)
-plt.plot(t,stb,":",c='black',zorder=2.0)
-plt.plot(np.zeros_like(t[1:]),t[1:],"--",c='black',zorder=2.0)
-plt.scatter(morita[:,0], morita[:,1], s=11**2, color=[0,0,0,0], marker='s', edgecolor='black', zorder=3.0)
-plt.scatter(moritaC[:,0], moritaC[:,1], s=11**2, c='black', marker='s', edgecolor='black', zorder=3.0)
-plt.scatter(bartholdi[:,0], bartholdi[:,1], s=17**2, c='gray', marker='H', zorder=3.0)
-plt.text(0.5,14,"$H_0(\operatorname{Out}(F_n)) = \mathbb{Q}$",backgroundcolor="white",zorder=4.0)
-plt.text(18,6,"$H_k(\operatorname{Out}(F_n)) = 0$ above vcd\n$k > 2n-3$",ha="center",backgroundcolor="white", zorder=4.0)
-plt.text(3.5,10,"$H_k(\operatorname{Out}(F_n)) = 0$\nin stable range\n$n \geq \\frac{5 (k+1)}{4}$",ha="center",backgroundcolor="white", zorder=0.9)
+#orange = [1.0,0.7,0.627,0.6]
+orange = [1.0,0.627,0.0,0.45]
+plt.fill_between(t2[:7],vcd[:7],stb[:7],color=orange,zorder=1.0)
+plt.fill_between(t2[6:],vcd[6:],stb[6:],color=[0.7,0.7,0.7,0.3],zorder=1.0)
 
-x = [0,24]
-y = [1,14]
-plt.xlim([-1,25])
-plt.ylim([0,15])
-plt.xticks(np.arange(x[0], x[1]+1, 1.0))
-plt.yticks(np.arange(y[0], y[1]+1, 1.0))
+plt.plot(t2,vcd,":",c='black',zorder=2.0)
+plt.plot(t2,stb,c='black',zorder=2.0)
+plt.plot(t,np.zeros_like(t),"--",c='black',zorder=2.0)
+
+plt.scatter(morita[:,0], morita[:,1], s=11**2, color=[0,0,0,0], marker='o', edgecolor='black', zorder=3.0)
+plt.scatter(moritaC[:,0], moritaC[:,1], s=11**2, c='black', marker='o', edgecolor='black', zorder=3.0)
+plt.scatter(bartholdi[:,0], bartholdi[:,1], s=13**2, c='gray', marker='v', zorder=3.0)
+
+plt.text(13.25,1,"$H_0(\operatorname{Out}(F_n)) = \mathbb{Q}$",backgroundcolor="white",zorder=4.0)
+plt.text(3.5,12,"$H_k(\operatorname{Out}(F_n)) = 0$\nabove vcd\n$k > 2n-3$",ha="center",backgroundcolor="white", zorder=4.0)
+plt.text(15.5,6,"$H_k(\operatorname{Out}(F_n)) = 0$ below\nstable range $k \leq \\frac{4}{5} n - 1$",ha="center",backgroundcolor="white", zorder=4.0)
+
+x = [1,20]
+y = [0,17]
+plt.xlim([x[0]-1,x[1]])
+plt.ylim([y[0]-1,y[1]])
+plt.xticks(np.arange(x[0], x[1]+1),np.arange(x[0], x[1]).tolist() + ['n'])
+plt.yticks(np.arange(y[0], y[1]+1),np.arange(y[0], y[1]).tolist() + ['k'])
 plt.grid(True,zorder=-1.0)
 ax = plt.gca()
 ax.set_axisbelow(True)
-ax.set_aspect('equal')
-plt.xlabel("k",labelpad = -14.5, loc='right')
-plt.ylabel("n",labelpad = -14.5, loc='top',rotation=0)
+#ax.set_aspect('equal')
+#plt.xlabel("n",labelpad = -14.5, loc='right')
+#plt.ylabel("k",labelpad = -14.5, loc='top',rotation=0)
 plt.savefig("OutFnHomology.pdf",bbox_inches='tight')
 
